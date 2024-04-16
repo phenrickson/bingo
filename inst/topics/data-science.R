7f27eba72da6682219146f5e21627022b80f4489
Phil Henrickson <phil.henrickson@github.com>
2024-04-16 09:43:01 -0500

    adding annoying questions in data science

---
+++ inst/topics/data-science.R
@@ -0,0 +1,16 @@
+# annoying questions you have heard from other data scientists, or executives
+c(
+  "What if we used artificial intelligence to find insights that promote efficiency?",
+  "Let's just ask ChatGPT and see what it thinks.",
+  "Can't you do all of this in Tableau?",
+  "We want to use AI. Show us how to use AI.",
+  "Why would we store historical data?",
+  "Accuracy is the only metric we care about.",
+  "If the predictions aren't what we like, we just adjust the coefficients. That's why we do all of our scoring in SQL.",
+  "Why can't we predict everything with 100% accuracy?",
+  "Can we just skip the data cleaning part?",
+  "You're too concerned about the science. Just make it look good",
+  "R is a dead language. Rebuild everything you've done in Python.",
+  "You care too much about the science. This is why we only hire computer scientists.",
+  "I need to evaluate this model myself. Can you rebuild the model in Excel so I can examine it?"
+)
